6f759ba170d4fbc75a6b5d52bc65f5dae8ac8128
Tiziano Titone <titone1996@gmail.com>
2024-05-02 10:50:55 +0200

    upd

--- Task_Auth/obj/Debug/net8.0/Task_Auth.AssemblyInfo.cs
+++ Task_Auth/obj/Debug/net8.0/Task_Auth.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task_Auth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c76be7c738b55c69e8650c66c8e0f540be724265")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task_Auth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task_Auth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
